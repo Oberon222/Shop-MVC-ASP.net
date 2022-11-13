98af82221c1cfb9a1727ab85136c99ae65b2c556
KostenkoPavlo <pavlokostenko222@gmail.com>
2022-11-13 23:38:41 +0200

    Create btn Create Category and table Categories

--- Shop-MVC2/Controllers/CategoryController.cs
+++ Shop-MVC2/Controllers/CategoryController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Shop_MVC2.Data;
+using Shop_MVC2.Models;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Shop_MVC2.Controllers
 {
   public class CategoryController : Controller
   {
+    private readonly ApplicationDBContext _db;
+
+    public CategoryController(ApplicationDBContext db)
+    {
+      _db = db; 
+    }
+
     public IActionResult Index()
     {
-      return View();
+      IEnumerable<Category> categoryList = _db.Categories;
+      return View(categoryList);
     }
   }
 }
